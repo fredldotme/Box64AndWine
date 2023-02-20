e20f2571adb3687bf8e43f780076ae3c4458ea38
Alfred Neumayer <dev.beidl@gmail.com>
2023-02-20 16:41:38 +0100

    wrappers: Commonalize env var prepend function

--- src/wrappers/common.h
+++ src/wrappers/common.h
@@ -5,24 +5,23 @@
 #include <stdlib.h>
 #include <string>
 
-inline void setupEnvironment() {
-    setenv("BOX64_LOG", "0", 1);
-    setenv("BOX64_NOBANNER", "1", 1);
-    setenv("BOX64_LD_LIBRARY_PATH", "/opt/click.ubuntu.com/box64andwine.fredldotme/current/lib/x86_64-linux-gnu", 1);
-
-    const char* ldp = getenv("LD_LIBRARY_PATH");
-    std::string newLdp = "/opt/click.ubuntu.com/box64andwine.fredldotme/current/lib/aarch64-linux-gnu";
+inline void prependEnvVar(const char* key, const char* value)
+{
+    const char* ldp = getenv(key);
+    std::string newLdp = std::string(value);
     if (ldp) {
         newLdp += (":" + std::string(ldp));
     }
-    setenv("LD_LIBRARY_PATH", newLdp.c_str(), 1);
+    setenv(key, newLdp.c_str(), 1);
+}
 
-    const char* path = getenv("PATH");
-    std::string newPath = "/opt/click.ubuntu.com/box64andwine.fredldotme/current/lib/aarch64-linux-gnu/bin";
-    if (path) {
-        newPath += (":" + std::string(path));
-    }
-    setenv("PATH", newPath.c_str(), 1);
+inline void setupEnvironment()
+{
+    setenv("BOX64_LOG", "0", 1);
+    setenv("BOX64_NOBANNER", "1", 1);
+    setenv("BOX64_LD_LIBRARY_PATH", "/opt/click.ubuntu.com/box64andwine.fredldotme/current/lib/x86_64-linux-gnu", 1);
+    prependEnvVar("LD_LIBRARY_PATH", "/opt/click.ubuntu.com/box64andwine.fredldotme/current/lib/aarch64-linux-gnu");
+    prependEnvVar("PATH", "/opt/click.ubuntu.com/box64andwine.fredldotme/current/lib/aarch64-linux-gnu/bin");
 }
 
 #endif
