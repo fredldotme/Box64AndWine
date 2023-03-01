9116237bf9d5830d10a5d774e0e207878907e3de
Alfred Neumayer <dev.beidl@gmail.com>
2023-03-01 18:42:06 +0100

    wrappers/box86: Add regular lib directory

--- src/wrappers/i386/box86.cpp
+++ src/wrappers/i386/box86.cpp
@@ -3,6 +3,8 @@
 
 int main(int argc, char** argv) {
     setupEnvironment();
-    setenv("LD_LIBRARY_PATH", "/opt/click.ubuntu.com/box64andwine.fredldotme/current/sysroot/armhf/lib/arm-linux-gnueabihf", 1);
+    setenv("LD_LIBRARY_PATH", "", 1);
+    prependEnvVar("LD_LIBRARY_PATH", "/opt/click.ubuntu.com/box64andwine.fredldotme/current/sysroot/armhf/usr/lib/arm-linux-gnueabihf");
+    prependEnvVar("LD_LIBRARY_PATH", "/opt/click.ubuntu.com/box64andwine.fredldotme/current/sysroot/armhf/usr/lib");
     return execvp("/opt/click.ubuntu.com/box64andwine.fredldotme/current/bin/box86", argv);
 }
